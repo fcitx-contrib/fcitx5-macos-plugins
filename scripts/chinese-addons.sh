5e2af4ff43e511ff0fcc9aaf0284bfebca1a6275
ksqsf <i@ksqsf.moe>
2023-12-28 12:36:18 +0800

    ci: try again

--- scripts/chinese-addons.sh
+++ scripts/chinese-addons.sh
@@ -1,37 +1,26 @@
 set -e
 . ./common.sh chinese-addons $1
+cd "$ADDON_ROOT"
 
-# Fix cross compile
-if [[ $ARCH != x86_64 ]]; then
-    ( cd "$ROOT/libime/data" && sed -i '' "s|COMMAND LibIME::|COMMAND /usr/local/bin/libime_|" CMakeLists.txt )
+
+ARGS=(
+    -DENABLE_TEST=OFF
+    -DENABLE_GUI=OFF
+)
+
+
+# HACK: Fix cross compile
+# Use prebuilt libime_* tools instead of generated ones
+if [[ $ARCH != `uname -m` ]]; then
     ( cd "$ADDON_ROOT/im/pinyin" && sed -i '' "s|COMMAND LibIME::|COMMAND /usr/local/bin/libime_|" CMakeLists.txt )
 fi
 
-############################
-# Build dependency: libime #
-############################
-cd "$ROOT/libime"
 
-f5m_configure -DENABLE_TEST=OFF
+# HACK: Disable tools
+sed -i '' 's/add_subdirectory.tools.//' CMakeLists.txt
+
+
+f5m_configure "${ARGS[@]}"
 f5m_build
 f5m_install
-
-###############################
-# Build fcitx5-chinese-addons #
-###############################
-cd "$ADDON_ROOT"
-sed -i '' 's/add_subdirectory.tools.//' CMakeLists.txt  # HACK: Disable tools
-f5m_configure -DENABLE_TEST=OFF \
-              -DENABLE_GUI=OFF \
-              -DLibIMECore_DIR=$ADDON_INSTALL_PREFIX/lib/cmake/LibIMECore/ \
-              -DLibIMEPinyin_DIR=$ADDON_INSTALL_PREFIX/lib/cmake/LibIMEPinyin/ \
-              -DLibIMETable_DIR=$ADDON_INSTALL_PREFIX/lib/cmake/LibIMETable/ \
-              -DCMAKE_PREFIX_PATH=$ADDON_INSTALL_PREFIX/../lua/
-f5m_build
-f5m_install
-git checkout CMakeLists.txt
-
-##########
-# Output #
-##########
 f5m_make_tarball
