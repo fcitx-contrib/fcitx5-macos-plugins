4ac7411f192d114302db556dcfbf1186bcc61f2c
ksqsf <i@ksqsf.moe>
2023-12-29 01:23:01 +0800

    ci: trigger

--- scripts/chinese-addons.sh
+++ scripts/chinese-addons.sh
@@ -2,23 +2,14 @@ set -e
 . ./common.sh chinese-addons $1
 cd "$ADDON_ROOT"
 
-
 ARGS=(
     -DENABLE_TEST=OFF
     -DENABLE_GUI=OFF
 )
 
-
-# HACK: Fix cross compile
-# Use prebuilt libime_* tools instead of generated ones
 if [[ $ARCH != `uname -m` ]]; then
-    ( cd "$ADDON_ROOT/im/pinyin" && sed -i '' "s|COMMAND LibIME::|COMMAND /usr/local/bin/libime_|" CMakeLists.txt )
+    ARGS+="-DENABLE_DATA=OFF"
 fi
-
-
-# HACK: Disable tools
-sed -i '' 's/add_subdirectory.tools.//' CMakeLists.txt
-
 
 f5m_configure "${ARGS[@]}"
 f5m_build
