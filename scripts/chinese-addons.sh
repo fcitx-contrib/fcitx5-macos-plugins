a9dfc5d1cd9b4d84ff43b3172e2edf66eab2ad33
Qijia Liu <liumeo@pku.edu.cn>
2023-12-28 22:33:45 -0500

    package dependencies to plugin

--- scripts/chinese-addons.sh
+++ scripts/chinese-addons.sh
@@ -10,7 +10,8 @@ if [[ $ARCH != $(uname -m) ]]; then
   ARGS+=("-DENABLE_DATA=OFF")
 fi
 
+install_deps libime
 f5m_configure "${ARGS[@]}"
 f5m_build
-f5m_install
+f5m_install libime
 f5m_make_tarball
