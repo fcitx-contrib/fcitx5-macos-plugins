a90c826fc74063f558f674f78c4095b0e9ee0c9d
Qijia Liu <liumeo@pku.edu.cn>
2024-01-05 18:43:13 -0500

    use prebuilt zstd

--- scripts/chinese-addons.sh
+++ scripts/chinese-addons.sh
@@ -3,7 +3,7 @@ set -e
 
 git reset --hard
 git apply ../patches/chinese-addons.patch
-install_deps libime marisa opencc
+install_deps libime marisa opencc zstd
 
 export LDFLAGS="-L$INSTALL_PREFIX/lib -lmarisa"
 
