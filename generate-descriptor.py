16bf6bb3861b39c03781a26135d47710961ae9a2
Qijia Liu <liumeo@pku.edu.cn>
2024-05-19 11:25:40 -0400

    add version to descriptor and generate meta-arch.json (#32)

--- generate-descriptor.py
+++ generate-descriptor.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 import sys
 
 input_methods = sys.argv[1:]
@@ -8,13 +9,25 @@ cwd = os.getcwd()
 plugin = cwd.split("/")[-3] # /path/to/rime/tmp/fcitx5
 files: list[str] = []
 
-for dirpath, _, filenames in os.walk(os.getcwd()):
+for dirpath, _, filenames in os.walk(cwd):
+    if dirpath == "plugin":
+        continue
     for filename in filenames:
         files.append(f"{dirpath[len(cwd) + 1:]}/{filename}")
+
+
+def getVersion():
+    # XXX: it causes unnecessary update when the plugin is not changed
+    result = subprocess.run(["git", "rev-parse", "HEAD"], stdout=subprocess.PIPE, text=True)
+    return result.stdout.strip()
+
+
+version = getVersion()
 
 os.makedirs("plugin", exist_ok=True)
 with open(f"plugin/{plugin}.json", "w") as f:
     descriptor = {
+        "version": version,
         "files": files
     }
     if input_methods:
